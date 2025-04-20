7a9cd51a438236daa690701cac057bfd1222a26a
FSReed <1714747596@qq.com>
2025-04-20 21:44:56 +0800

    Add 2 custom prompts

--- lua/plugins/codecompanion/prompts.lua
+++ lua/plugins/codecompanion/prompts.lua
@@ -1,8 +1,33 @@
 -- Custom prompts
 
 return {
-  ["Grammar Check"] = {
-    stragety = "chat",
-    description = "Check English Writing",
+  ["Improve Writing"] = {
+    strategy = "inline",
+    opts = {
+      modes = { "v" },
+      short_name = "grammar",
+      ignore_system_prompt = true,
+    },
+    prompts = {
+      {
+        role = "user",
+        content = [[Review the grammar of the provided text and enhance the writing.]],
+      },
+    },
+  },
+
+  ["Optimize"] = {
+    strategy = "inline",
+    description = "Optimize code",
+    opts = {
+      modes = { "v" },
+      short_name = "optimize",
+    },
+    prompts = {
+      {
+        role = "user",
+        content = [[Review the provided code, and optimize the code structure if necessary.]],
+      },
+    },
   },
 }

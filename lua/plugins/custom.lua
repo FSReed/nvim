fcb2367ea2ec31b7c1bec0f015d4cab644570187
FSReed <1714747596@qq.com>
2025-04-20 18:00:49 +0800

    Refactor code

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -5,8 +5,8 @@ return {
     -- The search count will appear randomly on the screen with scroll animation
     -- So I change search_count from virtualText to mini
     "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.routes = {
+    opts = {
+      routes = {
         {
           view = "mini",
           filter = {
@@ -14,25 +14,27 @@ return {
             kind = "search_count",
           },
         },
-      }
-    end,
+      },
+    },
   },
 
   {
     "Saghen/blink.cmp",
-    opts = function(_, opts)
+    opts = {
       -- I'm fed up with accepting auto-completions with <CR> 😣
-      opts.keymap.preset = "default"
-    end,
+      keymap = {
+        preset = "default",
+      },
+    },
   },
 
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
-  }
+  },
 }

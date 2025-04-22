29e69b8a3886640f4d0f90862e2503eb3e1953aa
FSReed <1714747596@qq.com>
2025-04-22 16:00:38 +0800

    Neovim WSL Clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,17 @@
 
 -- Customize winbar
 vim.o.winbar = "%=%m %f"
+
+-- WSL clipboard settings
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 1,
+}

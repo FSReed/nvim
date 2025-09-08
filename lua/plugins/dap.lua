d3cf66c2d2be0c785056c1b6494e495c76f16fe9
FSReed <1714747596@qq.com>
2025-09-08 17:40:27 +0800

    feat: key-mappings for dap-core

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<F5>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<F6>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F7>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+    },
+  },
+}

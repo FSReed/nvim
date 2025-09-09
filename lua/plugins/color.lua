dc240e28c2fac2a95e2396b3625256de8982ba74
FSReed <1714747596@qq.com>
2025-09-10 01:19:27 +0800

    feat(tokyonight): set a clearer border line color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -9,4 +9,13 @@ return {
       colorscheme = "tokyonight",
     },
   },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_colors = function(colors)
+        colors.border = "#565f89"
+      end,
+    },
+  },
 }

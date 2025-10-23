9ed58b23dd97fd22a78d5453beb1cd21abfcae14
FSReed <1714747596@qq.com>
2025-10-23 10:26:39 +0800

    feat(LSP): shutdown the LSP server before quit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,24 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local lspSafeQuitGroup = vim.api.nvim_create_augroup("LspSafeQuit", { clear = true })
+-- Create the autocmd that triggers on the "VimLeave" event.
+-- This event fires just before Neovim exits.
+vim.api.nvim_create_autocmd("QuitPre", {
+  group = lspSafeQuitGroup,
+  pattern = "*", -- Apply to all files and buffers.
+  desc = "Safely stop active LSP servers before quitting Neovim",
+  callback = function()
+    -- Get a list of all active LSP clients.
+    local active_clients = vim.lsp.get_clients()
+
+    -- Check if the list is not empty. The '#' operator gets the length of a table.
+    if #active_clients > 0 then
+      -- If there are active clients, execute the LspStop command to shut them down.
+      -- We wrap this in a protected call (pcall) so that if the LspStop
+      -- command were to fail for some reason, it wouldn't prevent Neovim from exiting.
+      vim.cmd("LspStop")
+    end
+  end,
+})
